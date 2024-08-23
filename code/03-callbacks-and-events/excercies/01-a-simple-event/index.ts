6ffe6c8796351b70948933caca7d74bab6c0666c
Arturo Abreu <r2work@pm.me>
2024-08-23 19:10:10 +0100

    exercise 03/01: a simple event

---
+++ code/03-callbacks-and-events/excercies/01-a-simple-event/index.ts
@@ -0,0 +1,53 @@
+import { readFile } from "fs";
+import { EventEmitter } from "events";
+import type { PathOrFileDescriptor } from "fs";
+
+interface EventsMap {
+  error: [Error];
+  read: [PathOrFileDescriptor];
+  match: [PathOrFileDescriptor, string, string];
+  start: [PathOrFileDescriptor[]];
+}
+
+class RegexFinder extends EventEmitter<EventsMap> {
+  private regex: RegExp;
+  private files: string[];
+
+  constructor(files: string[], regexPattern: string) {
+    super();
+
+    if (!regexPattern) this.emit("error", new Error("No regex provided"));
+    this.regex = new RegExp(regexPattern, "g");
+    this.files = files;
+  }
+
+  find() {
+    process.nextTick(() => this.emit("start", this.files));
+    for (const file of this.files) {
+      readFile(file, "utf-8", (error, data) => {
+        if (error) {
+          return this.emit("error", error);
+        }
+
+        this.emit("read", file);
+
+        const match = data.match(this.regex);
+
+        if (match) {
+          match.forEach((element) => this.emit("match", file, element, data));
+        }
+      });
+    }
+
+    return this;
+  }
+}
+
+const finder = new RegexFinder(["fileB.json", "fileA.txt"], process.argv[2]);
+
+finder
+  .find()
+  .on("start", (files) => console.log("Started", files))
+  .on("error", (error) => console.error(error))
+  .on("read", (file) => console.log(`${file} was read`))
+  .on("match", (file, data) => console.log(`Match on ${file} -> ${data}`));
