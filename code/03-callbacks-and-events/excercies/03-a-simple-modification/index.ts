d75def8d372695e3927d0995499f8656834fc7a4
Arturo Abreu <r2work@pm.me>
2024-08-23 19:27:38 +0100

    ercise 03/03: a simple modification

---
+++ code/03-callbacks-and-events/excercies/03-a-simple-modification/index.ts
@@ -0,0 +1,50 @@
+import { EventEmitter } from "events";
+
+interface EventsMap {
+  tick: [];
+  start: [];
+}
+
+(function (
+  timer: number,
+  callback: (error: Error | null, ticks: number) => void
+) {
+  const EMITTER = new EventEmitter<EventsMap>();
+  const MILLISECONDS_INTERVAL = 50;
+  let ticks = 0;
+  let elapsedTimeInMilliseconds = 0;
+  let intervalID: NodeJS.Timeout;
+
+  function start() {
+    process.nextTick(() => tick())
+    intervalID = setInterval(itirate, MILLISECONDS_INTERVAL);
+  }
+
+  function itirate() {
+    if (elapsedTimeInMilliseconds >= timer) return stop();
+
+    elapsedTimeInMilliseconds += MILLISECONDS_INTERVAL;
+    tick();
+  }
+
+  function tick() {
+    EMITTER.emit("tick");
+    ticks++;
+  }
+
+  function stop() {
+    clearInterval(intervalID);
+    return callback(null, ticks);
+  }
+
+  start();
+
+  return EMITTER;
+})(1000, (error, result) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`Number of ticks: ${result}`);
+  }
+})
+  .on("tick", () => console.log("Tick"));
